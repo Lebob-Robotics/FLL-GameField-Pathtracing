4524a692f15d56faff55a804fbb4068366ed73e1
Eason-F <eason.ycfeng@outlook.com>
2026-02-19 19:20:00 +0800

    feature: added grid and editing capabilities

--- scr/pathtracing/pathfinding/grid.py
+++ scr/pathtracing/pathfinding/grid.py
@@ -1,58 +1,38 @@
 import pygame
-import math
 
-from scr.pathtracing.pathfinding.node import Node
+from Node import Node
 
-class Grid:
-    def __init__(self, rows, cols, width):
-        # list comprehension to create grid
-        self.grid = [[Node(row, col, width, rows) for row in range(rows)] for col in range(cols)]
-
-        # store dimensions
-        self.width = width # window width
-        self.space = width // rows # space between nodes
-        self.rows = rows
-        self.cols = cols
-
-        # list of valid neighbours for each node with their relative positions
-        self.adjacent_neighbours = ((-1, 0), (0, 1), (1, 0), (0, -1))
-        self.diagonal_neighbours = ((-1, -1), (-1, 1), (1, 1), (1, -1))
-        self.diagonal_weight = math.sqrt(2) # weight for diagonal movement
-
-    def draw(self, surf):
-        surf.fill('white')
-        for col in self:
-            for node in col:
-                node.draw(surf)
-
-        # draw grid lines
-        for i in range(self.rows):
-            pygame.draw.line(surf, 'grey', (0, i * self.space), (self.width, i * self.space))
-            for j in range(self.cols):
-                pygame.draw.line(surf, 'grey', (j * self.space, 0), (j * self.space, self.width))
-
+class Grid(pygame.sprite.Group):
+    def __init__(self, length: int, height: int, windowSurface: pygame.Surface):
+        self.height: int = height
+        self.length: int = length
+        
+        self.windowSurface: pygame.Surface = windowSurface
+        self.nodeSize: tuple[float, float] = (self.windowSurface.get_width() / length, self.windowSurface.get_height() / height)
+        
+        self.nodes = [[Node(x, y, self.nodeSize[0], self.nodeSize[1], flag = Node.Flags.UNCHECKED) for y in range(height)] for x in range(length)]
+        
+    def draw(self, surface: pygame.Surface):
+        surface.fill('white')
+        for row in self.nodes:
+            for node in row:
+                rect = pygame.rect.Rect(
+                    node.x * self.nodeSize[0], node.y * self.nodeSize[1], 
+                    self.nodeSize[0], self.nodeSize[1])
+                pygame.draw.rect(surface, node.getFlag().value, rect)
+                
+        for row in range(len(self.nodes)):
+            for column in range(row):
+                pygame.draw.line(surface, "grey", (row * self.nodeSize[0], 0), (row * self.nodeSize[0], self.height * self.nodeSize[1]))
+                
+        for column in range(len(self.nodes[0])):
+            for row in range(column):
+                pygame.draw.line(surface, "grey", (0, column * self.nodeSize[1]), (self.length * self.nodeSize[0], column * self.nodeSize[1]))
+                
     def get_mouse_node(self, pos):
-        row = pos[1] // self.space
-        col = pos[0] // self.space
-        return self[col][row]
-
-    # check if position is within grid bounds
-    def within_bounds(self, col, row):
-        return 0 <= row < self.rows and 0 <= col < self.cols
-
-    def update_neighbours(self):
-        for col in self:
-            for node in col:
-                node.update_neighbours(self)
-
-    def update_heuristics(self, end):
-        for col in self:
-            for node in col:
-                node.h = self.heuristic(node, end)
-
+        x = int(pos[0] // self.nodeSize[0])
+        y = int(pos[1] // self.nodeSize[1])
+        return self[x][y]
+        
     def __getitem__(self, key):
-        return self.grid[key]
-
-    @staticmethod
-    def heuristic(node1, node2):
-        return math.dist((node1.col, node1.row), (node2.col, node2.row))
+        return self.nodes[key]
--- scr/pathtracing/pathfinding/node.py
+++ scr/pathtracing/pathfinding/node.py
@@ -1,60 +1,30 @@
-import pygame
+from pygame import sprite
 
-flags = {
-    'unchecked': 'white',
-    'checking': 'blue',
-    'open': 'green',
-    'closed': 'red',
-    'barrier': 'black',
-    'origin': 'orange',
-    'destination': 'aquamarine4',
-    'path': 'purple'
-}
+from enum import Enum
 
-class Node:
-    def __init__(self, row, col, width, total_rows):
-        self.row = row
-        self.col = col
-
-        self.width = width // total_rows
-        self.total_rows = total_rows
-
-        # positioning on 2D plane
-        self.x = col * self.width
-        self.y = row * self.width
-
-        # node state
-        self.colour = flags['unchecked']
-        self.flag = 'unchecked'
-
-        # algorithm related
-        self.neighbours = [] # 2D list of adjacent nodes with their respective weights
-        self.g = float('inf')
-        self.h = float('inf')
-        self.f = self.g + self.h
-
-    # state management
-    def is_state(self, state):
-        return self.flag == state
-
-    def set_state(self, state):
-        self.flag = state
-        self.colour = flags[state]
-
-    def reset_state(self):
-        self.set_state('unchecked')
-
-    def draw(self, surf):
-        pygame.draw.rect(surf, self.colour, (self.x, self.y, self.width, self.width))
-
-    def update_neighbours(self, grid):
-        self.neighbours = []
-        for neighbour in grid.adjacent_neighbours:
-            col, row = self.col + neighbour[0], self.row + neighbour[1]
-            if grid.within_bounds(col, row) and not grid[col][row].is_state('barrier'):
-                self.neighbours.append([grid[col][row], 1]) # where second value is weight
-
-        for neighbour in grid.diagonal_neighbours:
-            col, row = self.col + neighbour[0], self.row + neighbour[1]
-            if grid.within_bounds(col, row) and not grid[col][row].is_state('barrier'):
-                self.neighbours.append([grid[col][row], grid.diagonal_weight])
+class Node(sprite.Sprite):
+    class Flags(Enum):
+        EMPTY = 'pink'
+        UNCHECKED = 'white'
+        CHECKED = 'red'
+        OPEN = 'green'
+        BARRIER = 'black'
+        ORIGIN = 'orange'
+        DESTINATION = 'turqoise'
+        PATH = 'purple'
+    
+    def __init__(self, x: int, y: int, length: float, width: float = 0, flag = Flags.EMPTY):
+        self.x: int = x
+        self.y: int = y
+        self.length: float = length
+        self.width: float = width if width else length
+        self.flag = flag
+        
+    def setFlag(self, flag: Flags):
+        self.flag = flag
+        
+    def getFlag(self):
+        return self.flag
+    
+    def isFlag(self, flag: Flags):
+        return self.flag == flag
